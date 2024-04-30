977a9fe09f542423ee79200781e34c8629a7b8e1
Mahak Juriani <juriani.mahak@gmail.com>
2024-04-30 23:21:06 +0530

    Second Commit

---
+++ src/main/java/org/example/models/Status.java
@@ -0,0 +1,7 @@
+package org.example.models;
+
+public enum Status {
+    ON_GOING,
+    PENDING,
+    FINISHED;
+}
---
+++ src/main/java/org/example/models/User.java
@@ -0,0 +1,13 @@
+package org.example.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    Long id;
+    String name;
+    Long phNum;
+
+}
